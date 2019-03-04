2cf0839420c4b187bd5bfbe6bafe043a8468a59e
Farida <1>
2019-03-04 18:47:27 +0300

    task4 added

--- lib.py
+++ lib.py
@@ -4,5 +4,14 @@ def normalize(data, n):
         del data[ind_max]
         ind_min = data.index(min(data))
         del data[ind_min]
-
     return data
+
+def uniq(data):
+    result = []
+    for element in data:
+        if element not in result:
+            result.append(element)
+        else:
+            ind = result.index(element)
+            del result[ind]
+    return result
---
+++ task4.py
@@ -0,0 +1,4 @@
+from lib import uniq
+
+data = [2, 15, 28, 100, 2, 0, 15, 1]
+print(uniq(data))
